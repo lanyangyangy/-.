0c1b8bf933bad0ae97e92d3bd5c5145751c1e703
谭竣文 <92668369+lanyangyangy@users.noreply.github.com>
2022-03-13 00:30:28 +0800

    Update 进制转换.cpp

--- 进制转换.cpp
+++ 进制转换.cpp
@@ -1,29 +1,57 @@
-﻿# include<stdio.h>
+# include<stdio.h>
 #include "zhancaozuo.h"
-
 #include<string.h>
-void  shuru(int * n,long* shuzi);
+void  shuru(int* m,int * n,char* shuzi);
+long zhuanhuan(char* shuzi,int m);
 void ruzhan(Stack* zhan,long shuzi,int n);
 void chuzhan(Stack* zhan);
 
 int main()
 {
 	Stack xiaochu;
-	int n = 0;
-	long shuzi = 0;
+	int n = 0,m;
+	char shuzi[10] ;
 	InitStack(&xiaochu);
-	shuru(&n,&shuzi);
-	ruzhan(&xiaochu, shuzi, n);
+	shuru(&m,&n,shuzi);
+	
+	long shu = zhuanhuan(shuzi,m);
+	ruzhan(&xiaochu, shu, n);
 	chuzhan(&xiaochu);
 	return 0;
 }
 
-void shuru(int* n,long* shuzi)
+void shuru(int * m,int* n,char* shuzi)
 {
+
+	printf("输入的是几进制？\n");
+	scanf_s("%d", m);
+	int a=getchar();
 	printf("请问你想要转换成几进制：\n");
 	scanf_s("%d", n);
+	int b = getchar();
 	printf("输入你要转化的数：\n");
-	scanf_s("%ld", shuzi);
+	gets_s(shuzi, 10);
+}
+long zhuanhuan(char* shuzi,int m)
+{
+	long shu = 0,tidai=0;
+	for (int i =0 ; i <strlen(shuzi) ; i++)
+	{
+		if (shuzi[i] >= 48 && shuzi[i] <= 58)
+		{
+			tidai= shuzi[i] - 48;
+		}
+		else if (shuzi[i] >= 65 && shuzi[i] <= 91)
+		{
+			tidai = shuzi[i] - 65+10;	
+
+		}	
+		shu *= m;
+		shu += tidai;
+		tidai = 0;
+	}
+
+	return shu;
 }
 void  ruzhan(Stack* zhan,long shuzi,int n)
 {
@@ -61,23 +89,4 @@ void chuzhan(Stack* zhan)
 }
 
 
-//int main()
-//{
-//	Stack xiaochu;
-//	InitStack(&xiaochu);
-//	int i = 0;
-//	while (i < 5)
-//	{
-//		PushStack(&xiaochu, i);
-//		printf("%d", i);
-//		i++;
-//	}
-//	PopStack(&xiaochu);
-//	PopStack(&xiaochu);
-//	PopStack(&xiaochu);
-//	PopStack(&xiaochu);
-//	PopStack(&xiaochu);
-//	PopStack(&xiaochu);
-//
-//	return 0;
-//}
+
